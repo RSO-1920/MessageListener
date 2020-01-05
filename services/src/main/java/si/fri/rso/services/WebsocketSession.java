c74d89b8768ec422c1a129337144b78af2e27da1
Uroš Zoretič <uz1734@student.uni-lj.si>
2020-01-05 19:06:23 +0100

    it works

--- services/src/main/java/si/fri/rso/services/WebsocketSession.java
+++ services/src/main/java/si/fri/rso/services/WebsocketSession.java
@@ -1,5 +1,7 @@
 package si.fri.rso.services;
 
+import org.json.JSONObject;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.Dependent;
 import javax.websocket.Session;
@@ -15,11 +17,18 @@ public class WebsocketSession {
     public void onServerMessage(String message) throws IOException {
         System.out.println("on server message: " + message);
 
+        JSONObject jsonObject = new JSONObject(message);
+
+        String channel = Integer.toString((int) jsonObject.getInt("channelId"));
+        //String channel = "3";
+        System.out.println("Cgannel " + channel);
+
         try {
             for (Map.Entry<String, Session> entry : sessions.entrySet()) {
-                System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
-                System.out.println("is open: " + entry.getValue().isOpen());
-                if (entry.getValue().isOpen()) {
+                System.out.print("Key = " + entry.getKey() + ", Value = ");
+                System.out.println("     [MSG] is open: " + entry.getValue().isOpen());
+                if (entry.getValue().isOpen() && entry.getKey().equals(channel)) {
+                    System.out.println("Sending to " + entry.getKey() + "!");
                     entry.getValue().getBasicRemote().sendText(message);
                 }
             }
@@ -33,8 +42,8 @@ public class WebsocketSession {
         this.sessions.put(key, session);
         System.out.println("adding session");
         for (Map.Entry<String, Session> entry : sessions.entrySet()) {
-            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
-            System.out.println("is open: " + entry.getValue().isOpen());
+            System.out.println("Key = " + entry.getKey() + ", Value = ");
+            System.out.println("[ADD] is open: " + entry.getValue().isOpen());
         }
     }
 }
